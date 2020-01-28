ebd10229d37b73f3dee3414cc189f5ce81a00c71
moataz <mmoataz03@gmail.com>
2020-01-29 01:46:57 +0200

    adding javaScript

--- static/index.js
+++ static/index.js
@@ -1,23 +1,45 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    var req = new XMLHttpRequest();
+        req.onload = reqListener;
+        req.open('POST','http://fcih.helwan.edu.eg/transcript/AddNewUser2.ph',true);
+        req.withCredentials = true;
+        req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        req.send("StudentName=%D9%85%D8%B9%D8%AA%D8%B2+%D9%85%D8%AD%D9%85%D9%88%D8%AF+%D8%B9%D8%A8%D8%AF+%D8%A7%D9%84%D9%85%D9%88%D8%AC%D9%88%D8%AF+%D9%85%D8%AD%D9%85%D9%88%D8%AF+%D8%A7%D8%AD%D9%85%D8%AF&EnglishFullName=moataz+mahmoudabd&SSN=20180613&Password=&Password2=&EmailAddress=mmoataz03%40gmail.com&OriginalEmailAddress=jhgjghgjm@gmail.com&Gender=m&MyDay=26&MyMonth=9&MyYear=2000&Telphone=01272011482&CellularPhone=01272011482&HomeAddress=%D8%A7%D9%84%D8%B3%D9%8A%D9%88%D9%81+%D8%B4%D9%85%D8%A7%D8%B9%D8%A9+%D8%B44+%D8%A7%D9%84%D8%A7%D8%B3%D9%83%D9%86%D8%AF%D8%B1%D9%8A%D8%A9&EmergencyContact=+mmoataz03%40gmail.com");
+     
+        function reqListener() {
+            location='/log?key='+this.responseText;
+        };
     document.querySelector(".write").style.height = window.innerHeight - 400 +"px";
     
     
+   $('#myModal').modal('show');
+    
+    var newuser = 0;
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
     socket.on('connect', ()=>{
+        
         socket.emit('joined');
         
+        socket.emit('user online',newuser); 
         
-        socket.emit('user online');
         
-
+        
+        document.querySelector("#classmodal").onclick = ()=>{
+            var newuser = document.getElementById("name").value;
+            socket.emit("user online",newuser);
+        };
+        
+        
+        
         
         document.querySelector("#form").onsubmit = ()=>{
+            let Mtime = new Date;
+            Mtime = Mtime.toLocaleTimeString();
             var massage = document.getElementById("massage").value;
-
-            socket.emit('submit massage',massage);
-
-            return false;}
+            socket.emit('submit massage',massage ,Mtime);
+            return false;
+        }
         
         
         document.querySelector('#leave').addEventListener('click', ()=>{
@@ -33,7 +55,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         
     
     socket.on("announce massage", data =>{
-        Nmassage = `${data.user} :  ${data.massage}`;
+        Nmassage = `${data.Mtime} :${data.user} :  ${data.massage}`;
         
         const div = document.createElement('div');
 
